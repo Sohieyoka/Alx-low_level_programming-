5a89f1b82e2ad43111cd80f6d11d0f3f5abe5e0c
sohieyoka <ro2002137@gmail.com>
2023-06-20 16:02:37 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
+root@f04d8c062bed:~/alx-low_level_programming# rm 0-putchar.c
+root@f04d8c062bed:~/alx-low_level_programming# ls
+0x00-hello_world  0x01-variables_if_else_while  0x02-functions_nested_loops  README.md
+root@f04d8c062bed:~/alx-low_level_programming# cd 0x02-functions_nested_loops
+root@f04d8c062bed:~/alx-low_level_programming/0x02-functions_nested_loops# vi 0-putchar.c
+root@f04d8c062bed:~/alx-low_level_programming/0x02-functions_nested_loops#  cat 0-putchar.c
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
